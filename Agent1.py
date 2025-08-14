239d28c0f92d94fe4dec5faf15528793c528ba4d
imon333 <imonhosen99@gmail.com>
2025-08-14 23:25:08 +0200

    agent 1 is complete and then the exercise

---
+++ Agent1.py
@@ -0,0 +1,29 @@
+from typing import  TypedDict
+from langgraph.graph import StateGraph
+
+# part follow--
+
+class AgentState(TypedDict): 
+    message: str
+    
+    
+def greeting_node(state: AgentState) -> AgentState:
+    state['message'] = "Hello " + state['message']+ ",how can I assist you today?"
+    return state   
+
+graph = StateGraph(AgentState)
+
+graph.add_node("greeting", greeting_node)
+graph.set_entry_point("greeting")
+graph.set_finish_point("greeting")
+
+app = graph.compile()
+
+from IPython.display import Image, display 
+display(Image(app.get_graph().draw_mermaid_png()))
+
+output = app.invoke({"message": "imon"})
+
+output['message']  
+
+# exercise 
