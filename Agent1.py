30f7e934f7e6a26da552625a173916aad520007f
imon333 <imonhosen99@gmail.com>
2025-08-14 23:54:08 +0200

    agent 1 __ the exercise is done also

--- Agent1.py
+++ Agent1.py
@@ -19,11 +19,36 @@ graph.set_finish_point("greeting")
 
 app = graph.compile()
 
-from IPython.display import Image, display 
-display(Image(app.get_graph().draw_mermaid_png()))
+#from IPython.display import Image, display 
+#display(Image(app.get_graph().draw_mermaid_png()))
 
 output = app.invoke({"message": "imon"})
 
 output['message']  
 
 # exercise 
+
+class AgentState(TypedDict): 
+    message: str
+    complement: str
+    
+    
+def greeting_node(state: AgentState) -> AgentState:
+    state['message'] += "Hello " + state['message'] + state['complement']
+
+    
+    return state   
+
+graph = StateGraph(AgentState)
+
+graph.add_node("greeting", greeting_node)
+graph.set_entry_point("greeting")
+graph.set_finish_point("greeting")
+
+app = graph.compile()
+
+
+
+output = app.invoke({"message": "imon", "complement": ", you are the best"})
+
+output['message']  
